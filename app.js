ff0d50920cd7a7328958495485593b6c0c2fae63
mands-mands <mands.cristiny@gmail.com>
2025-08-16 20:37:43 -0300

    Adiciona novo botão e novas funções

--- app.js
+++ app.js
@@ -27,6 +27,23 @@ function sortearAmigo(){
         let resultado = document.getElementById(`resultado`);
         let amigoSecreto = Math.floor(Math.random() * tamanhoLista);
         resultado.innerHTML = `O seu amigo secreto é: ${participantes[amigoSecreto]}`
+        let sortearNovamente = document.getElementById('sortear');
+        sortearNovamente.innerHTML = `<img src="assets/play_circle_outline.png" alt="Ícone para sortear"> Sortear de novo`;
+        let limparLista = document.getElementById('limparLista');
+        limparLista.style.display = 'inline';
     }
     amigoInput.focus();
+    
+}
+
+function novaLista(){
+    let limparLista = document.getElementById('limparLista');
+    let resultado = document.getElementById('resultado');
+    let listaAmigos = document.getElementById('listaAmigos');
+    let sortearNovamente = document.getElementById('sortear');
+    limparLista.style.display = 'none';
+    participantes = [];
+    resultado.innerHTML = ``;
+    listaAmigos.innerHTML = ``;
+    sortearNovamente.innerHTML = `<img src="assets/play_circle_outline.png" alt="Ícone para sortear"> Sortear amigo`;
 }
